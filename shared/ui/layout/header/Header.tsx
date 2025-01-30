2dddeaf2f477e874badb04db25417e0bdb626b66
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2025-01-31 02:13:18 +0900

    creatTag 빌드 오류 해결 테스트트

--- shared/ui/layout/header/Header.tsx
+++ shared/ui/layout/header/Header.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { throttle } from 'lodash';
-// import Image from 'next/image';
-// import logoImg from"@/public/logo.png";
 import styles from "./header.module.scss";
 import cs from "classnames/bind";
 const cx = cs.bind(styles);
@@ -11,55 +9,48 @@ export interface navProps {
   title: string;
   link: string;
 }
-interface HeaderProps{
-  slug:string
+interface HeaderProps {
+  slug: string;
 }
-export const Header = ( props: HeaderProps ) => {
-  const {slug} = props;
+
+export const Header = (props: HeaderProps) => {
+  const { slug } = props;
   const nav: Array<navProps> = [
-    {title:'Home', link:'/'},
-    {title:'Projects', link:'/projects'},
-    {title:'Contact', link:'/contactme'},
-  ]
+    { title: 'Home', link: '/' },
+    { title: 'Projects', link: '/projects' },
+    { title: 'Contact', link: '/contactme' },
+  ];
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
+    if (typeof window === "undefined") return; // 서버에서 실행되지 않도록 방어 코드 추가
+
     const handleScroll = throttle(() => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }, 100); 
-  
+      setScrolled(window.scrollY > 0);
+    }, 100);
+
     window.addEventListener('scroll', handleScroll);
-  
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-
   return (
-    <>
-      <header className={cx("header__wrapper", { "is-scrolled": scrolled })}>
-        <div className={cx("header__inner")}>
-          <h1>
-            <Link href="./" className={cx("header__logo")}>NK_Lee</Link>
-          </h1>
-          <nav className={cx("nav__wrapper")}>
-            <ul className={cx("nav__menu")}>
-              {nav.map((item) => (
-                <li key={item.link} className={cx("nav__menu__item", slug == item.link.split("/").pop() ? "active": undefined)}>
-                  <Link href={item.link}>
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </>
+    <header className={cx("header__wrapper", { "is-scrolled": scrolled })}>
+      <div className={cx("header__inner")}>
+        <h1>
+          <Link href="/" className={cx("header__logo")}>NK_Lee</Link>
+        </h1>
+        <nav className={cx("nav__wrapper")}>
+          <ul className={cx("nav__menu")}>
+            {nav.map((item) => (
+              <li key={item.link} className={cx("nav__menu__item", slug == item.link.split("/").pop() ? "active" : undefined)}>
+                <Link href={item.link}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
