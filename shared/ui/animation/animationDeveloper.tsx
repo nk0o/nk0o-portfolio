788f083f309e4e9610cc9c5cb8c5056887b4b561
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2025-01-31 02:35:45 +0900

    lottie 동적 로딩딩

--- shared/ui/animation/animationDeveloper.tsx
+++ shared/ui/animation/animationDeveloper.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React from 'react'
+import React from 'react';
+import dynamic from 'next/dynamic';
 
-import Lottie from 'react-lottie-player'
-import lottieJson from '@/public/lottie_animation.json'
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
+
+import lottieJson from '@/public/lottie_animation.json';
 
 export default function AnimationDeveloper() {
   return (
-    <Lottie className="img-animtaion-developer"
-      loop
-      animationData={lottieJson}
-      play
-    />
-  )
+    <div className="img-animation-developer">
+      <Lottie loop animationData={lottieJson} play />
+    </div>
+  );
 }
