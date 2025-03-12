8712bd41d7c4580a4dab6119d706dc3817fb17dd
p-skrud714 <skrud714@grovesoft.net>
2025-03-12 18:41:00 +0900

    env: "eslint, prettier설정추가" feat: "Gtag 추가"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
-import type { Metadata } from "next";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
-import "@/styles/globals.scss";
-import { ReactNode } from "react";
-import { DefaultLayout } from "@/shared/ui/layout/DefaultLayout";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import type { Metadata } from 'next';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import '@/styles/globals.scss';
+import { ReactNode } from 'react';
+import { DefaultLayout } from '@/shared/ui/layout/DefaultLayout';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 export const metadata: Metadata = {
-  title: "이나경의 포트폴리오",
-  description: "NextJs로 만든 포트폴리오입니다.",
+  title: '이나경의 포트폴리오',
+  description: 'NextJs로 만든 포트폴리오입니다.',
 };
 
-export default function RootLayout({children,}: {children: ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <body>
-          <DefaultLayout>
-            {children}
-          </DefaultLayout>
+        <GoogleTagManager gtmId="	GTM-W43HQNDS" />
+        <DefaultLayout>{children}</DefaultLayout>
       </body>
-
     </html>
   );
 }
