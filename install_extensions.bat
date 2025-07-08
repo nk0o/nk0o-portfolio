0ec071be7745e57a006414f8cfc378e0bba5a2d9
p-skrud714 <skrud714@grovesoft.net>
2025-07-08 14:32:28 +0900

    vscode 익스텐션 리스트

---
+++ install_extensions.bat
@@ -0,0 +1,59 @@
+@echo off
+echo Installing VS Code extensions for Cursor...
+
+code --install-extension atlassian.atlascode
+code --install-extension bestvow.css-compressor
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension christian-kohler.path-intellisense
+code --install-extension cliffordfajardo.hightlight-selections-vscode
+code --install-extension danielsanmedium.dscodegpt
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.jquerysnippets
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension evan-buss.font-switcher
+code --install-extension febean.vue-format
+code --install-extension figma.figma-vscode-extension
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension glenn2223.live-sass
+code --install-extension hollowtree.vue-snippets
+code --install-extension htmlhint.vscode-htmlhint
+code --install-extension humy2833.ftp-simple
+code --install-extension jsonchou.css-compact
+code --install-extension kimseungtae.aicodehelper
+code --install-extension kisstkondoros.vscode-gutter-preview
+code --install-extension mhutchie.git-graph
+code --install-extension miladfathy.scss-snippet
+code --install-extension misterj.vue-volar-extention-pack
+code --install-extension mrmlnc.vscode-scss
+code --install-extension ms-ceintl.vscode-language-pack-ko
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension octref.vetur
+code --install-extension oderwat.indent-rainbow
+code --install-extension pkief.material-icon-theme
+code --install-extension pranaygp.vscode-css-peek
+code --install-extension redhat.vscode-community-server-connector
+code --install-extension redhat.vscode-rsp-ui
+code --install-extension redhat.vscode-yaml
+code --install-extension ritwickdey.liveserver
+code --install-extension sainoba.px-to-rem
+code --install-extension samuel-weinhardt.vscode-jsp-lang
+code --install-extension sdras.vue-vscode-snippets
+code --install-extension sibiraj-s.vscode-scss-formatter
+code --install-extension simonsiefke.svg-preview
+code --install-extension snehaljha.local-tomcat
+code --install-extension solnurkarim.html-to-css-autocompletion
+code --install-extension styled-components.vscode-styled-components
+code --install-extension syler.sass-indented
+code --install-extension techer.open-in-browser
+code --install-extension vue.volar
+code --install-extension wscats.vue
+code --install-extension xabikos.javascriptsnippets
+
+echo.
+echo Extension installation completed!
+pause 
